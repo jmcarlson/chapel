32df1159fd4401c80c54dac5b25094165d8b8d2f
Jon Carlson <jon@jomca.co>
2014-07-24 18:16:11 -0600

    Core form complete in Jade

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
-var server = app.listen(5265, function() {
+var server = app.listen(3000, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
