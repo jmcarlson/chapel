119edce0b2bd7eb10bc3d4a86b06835dcc9bbbd5
Jon Carlson <jon@jomca.co>
2014-08-01 09:05:06 -0600

    bodyParser.urlencoded update

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/crm', controller.index);
 app.post('/write', controller.write);
