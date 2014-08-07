3d8d4347b1598977f3dd3a58c62c25b00ad085d5
Jon Carlson <jon@jomca.co>
2014-08-07 14:56:20 -0700

    Backbone updates

--- public/views/lead-view.js
+++ public/views/lead-view.js
@@ -1,8 +1,9 @@
-var LeadView = Backbone.View.extend({
+var LeadViewHeader = Backbone.View.extend({
 	tagName: 'div',
+	className: 'debug1 core-data col-xs-12 col-sm-12 col-md-12',
 	// Put id of script section here
 	// template: _.template( $('.primary').html() ),
-	template: _.template('<div class="core-data"><%= cd01 %></div>'),
+	template: _.template('<div class="icon-inline icon-menu col-xs-12 col-sm-1 col-md-1"></div><div class="col-md-2"><%= cd01 %></div><div class="col-md-2"><%= cd02 %></div><div class="col-md-3"><%= cd03 %></div><div class="col-md-2"><%= cd04 %></div>'),
 
 	render: function() {
 		this.$el.html( this.template(this.model.toJSON()) );
@@ -11,30 +12,42 @@ var LeadView = Backbone.View.extend({
 
 });
 
+var LeadViewDetail = Backbone.View.extend({
+	tagName: 'div',
+	className: 'user-data',
+	template: _.template('<div class="debug2 col-xs-4 col-sm-4 col-md-2">First Name</div><div class="col-xs-8 col-sm-8 col-md-10 user-detail-data"><%= cd01 %></div>'),
+	render: function() {
+		this.$el.html( this.template(this.model.toJSON) );
+		return this;
+	}
+});
+
 var LeadListView = Backbone.View.extend({
 	el: '.primary',
 
 	// Use initialize to prime the pump
 	initialize: function() {
-		console.log('LeadListView init');
 		this.collection = new LeadList();
 		this.listenTo(this.collection, 'all', this.render);
 		this.collection.fetch({reset: true});
 },
 
 	render: function() {
-		console.log('LeadListView render');
 		this.collection.forEach(function(lead) {
 			this.renderList(lead);
 		}, this);
 	},
 
 	renderList: function(lead) {
-		var leadView = new LeadView({
+		var leadViewHeader = new LeadViewHeader({
 			model: lead
 		});
-		console.log(leadView.render().el);
-		this.$el.append(leadView.render().el);
+		var leadViewDetail = new LeadViewDetail({
+			model: lead
+		});
+		console.log(leadViewDetail);
+		this.$el.append(leadViewHeader.render().el);
+		this.$el.append(leadViewDetail.render().el);
 	}
 
 
