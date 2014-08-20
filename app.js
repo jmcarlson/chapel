75b89db5eb3d28cdf6fe5b6ac799ef2f8f554646
Jon Carlson <jon@jomca.co>
2014-08-19 18:24:58 -0600

    bodyParser.json disabled

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/crm', controller.index);
