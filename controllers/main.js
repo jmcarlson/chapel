317385c799cc5a96810f7194a0fb85df6ea36764
Jon Carlson <jon@jomca.co>
2014-08-20 15:59:39 -0600

    Updates

--- controllers/main.js
+++ controllers/main.js
@@ -295,9 +295,9 @@ var controller = {
 		// console.log('new phone: ', temp7);
 		var phone = temp3[0].replace(email[0],'').replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
 		// Parse out property notes
-		console.log('req.body.text', req.body.text);
-		console.log('tempx: ', tempx);
-		// var temp4 = tempx.split('For Sale: ');
+		// console.log('req.body.text', req.body.text);
+		// console.log('tempx: ', tempx);
+		var temp4 = tempx.split('For Sale: ');
 		// var temp5 = temp4[1].split('Listed by:');
 		// console.log('Notes: ',temp5[0]);
 
